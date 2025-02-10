a5d3f473f9172eb2516aec1a115438ffe584979d
Tracy Emperatriz Silva Flores <trsilva-@c2r17s4.42madrid.com>
2025-02-10 20:33:51 +0100

    first additions

---
+++ ft_printf.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trsilva- <trsilva-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 20:01:09 by trsilva-          #+#    #+#             */
+/*   Updated: 2025/02/10 20:29:52 by trsilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+va_start(va_list var, parameterN)
+{
+    
+}
+
+va_arg(va_list var, type_variable)
+{
+    
+}
+
+va_end(va_list arguments)
+{
+    
+}
+
+int ft_printf(char const *, ...)
+{
+    va_list arguments;
+}
+
