4b0eeb0798da9c5575ce8b637bcf4e0263c8c2b6
Tracy <tracysilva01@gmail.com>
2025-02-15 22:12:19 +0100

    some lines

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: trsilva- <trsilva-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 20:01:09 by trsilva-          #+#    #+#             */
-/*   Updated: 2025/02/15 01:18:24 by trsilva-         ###   ########.fr       */
+/*   Updated: 2025/02/15 22:08:46 by trsilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,17 @@
 void    format_specifier(char c, va_list arguments)
 {
     if (c == 'c')
-    {
         putchar(va_arg(arguments, int));
-    }
     else if (c == 's')
-    {
         putstring(va_arg(arguments, char *));
-    }
     else if (c == 'p')
-    {
         putpointer(va_arg(arguments, void *));
-    }
     else if (c == 'd' || c == 'i')
-    {
         putnumber(va_arg(arguments, int));
-    }
-    else if (c == 'u' || c = 'x' || c = 'X')
-    {
+    else if (c == 'u' || c == 'x' || c == 'X')
         putnumber(va_arg(arguments, unsigned int));
-    }
     else if (c == '%')
-    {
         putchar('%');
-    }
 }
 
 int ft_printf(char const *man_arg, ...)
@@ -52,17 +40,8 @@ int ft_printf(char const *man_arg, ...)
     {
         if (man_arg[i] == '%' && man_arg[i+1])
         {
-            if (man_arg[i+1] == 'c' || man_arg[i+1] == 's' ||
-                    man_arg[i+1] == 'p' || man_arg[i+1] == 'd'
-                    || man_arg[i+1] == 'i' || man_arg[i+1] == 'u'
-                    || man_arg[i+1] == 'x' || man_arg[i+1] == 'X'
-                    || man_arg[i+1] == '%')
-                     {
-                        format_specifier(man_arg[i+1], arguments);
-                        i += 2;
-                     }
-            else
-                putchar(man_arg[i++]);
+            format_specifier(man_arg[i+1], arguments);
+            i += 2;
         }
         else
             putchar(man_arg[i++]);
